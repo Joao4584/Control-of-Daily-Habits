4a6ca51a24347ed59e4e65cd8cbb2781cb3a0ae3
Joao Roberto <joaoroberto4584@gmail.com>
2023-01-25 23:17:03 -0300

    new contents and server conclused.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,31 +1,14 @@
-import Fastify from 'fastify';
-import cors from '@fastify/cors';
-import { PrismaClient } from '@prisma/client';
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import { appRoutes } from "./routes"
 
-const app = Fastify();
-const prisma = new PrismaClient();
+const app = Fastify()
 
-app.register(cors);
-
-/**
- *  Método HTTP: Get, Post, Put, Patch, Delete
- */
-
-app.get('/hello', async () => {
-    const habits = await prisma.habit.create({
-        where: {
-            title: {
-                startsWith: 'Beber'
-            }
-        }
-    })
-
-    return habits
-})
-
+app.register(cors)
+app.register(appRoutes)
 
 app.listen({
-    port: 4584,
+  port: 4584,
 }).then(() => {
-    console.log('HTTP Server running!');
+  console.log('HTTP Server running!')
 })
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,18 +1,15 @@
 import './styles/global.css';
 
-import { Habit } from "./components/Habit";
+import { Habit } from "./components/Habit"
 
 function App() {
- 
   return (
-   <div>
-     <Habit completed={4} />
-     <Habit completed={2} />
-     <Habit />
-     <Habit completed={6} />
-     <Habit completed={10} />
-     <Habit completed={23} />
-   </div>
+    <div>
+      <Habit completed={3} />
+      <Habit completed={10} />
+      <Habit completed={20} />
+      <Habit completed={30} />
+    </div>
   )
 }
 
